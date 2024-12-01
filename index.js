10d9944a32d8b3bc50e0e395e6ffc67e50ae8386
CarmenMenendez <ccitamr@gmail.com>
2024-12-01 16:26:32 +0100

    crear servidor y conectar con BD

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log(Hola);
+const express = require("express");
+require("dotenv").config();
+const connectDB = require("./src/utils/db_mongo");
+connectDB();
+
+
+
+const server = express();
+server.use(express.json);
+
+
+
+
+const PORT = process.env.PORT;
+
+server.listen (PORT, () =>{
+    console.log(`Server running port http://localhost:${PORT}`);
+});
